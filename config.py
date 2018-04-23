97dc81812bef2b1cf42b7ca5b7762dd4635a1fe8
Mario2334 <sanketm221995@yahoo.co.in>
2018-04-23 19:03:34 +0530

    yo

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 
 
-MONGO_HOST = 'localhost'
-MONGO_PORT = 27017
-
-MONGO_DBNAME = 'bank_det'
+# MONGO_HOST = 'localhost'
+# MONGO_PORT = 27017
+MONGO_uri = 'mongodb://<dbuser>:<dbpassword>@ds255539.mlab.com:55539/heroku_3xgdc44r'
+MONGO_DBNAME = 'heroku_3xgdc44r'

08eaf51f30c522694cb6bdca6a7b951c9bdc436f
Mario2334 <sanketm221995@yahoo.co.in>
2018-04-23 19:25:25 +0530

    yo

--- config.py
+++ config.py
@@ -2,5 +2,5 @@
 
 # MONGO_HOST = 'localhost'
 # MONGO_PORT = 27017
-MONGO_uri = 'mongodb://<dbuser>:<dbpassword>@ds255539.mlab.com:55539/heroku_3xgdc44r'
+MONGO_uri = 'mongodb://sanketm:sanket123@ds255539.mlab.com:55539/heroku_3xgdc44r'
 MONGO_DBNAME = 'heroku_3xgdc44r'
